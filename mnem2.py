04ce21ece1b74317123aeeb85007f542a5ebc497
Andrew Williams <williams.ab@gmail.com>
2025-06-20 10:09:59 +0200

    Replace Mnemonic with bip_uitls. Working! Remove debug code later.

--- mnem2.py
+++ mnem2.py
@@ -11,7 +11,7 @@ import argparse
 from mnemonic import Mnemonic
 import hashlib
 import hmac
-from bip_utils import Bip39MnemonicGenerator, Bip39SeedGenerator, Bip39WordsNum, Bip39Languages
+from bip_utils import Bip39MnemonicGenerator, Bip39SeedGenerator, Bip39WordsNum, Bip39Languages, Bip39MnemonicValidator
 
 '''
 # Generate mnemonic
@@ -84,38 +84,54 @@ def main():
     '''
 
     language = check_list(args.language, language_list)
-    # logging.info(f"The checked and parsed language is ==> {language} <==\n")
+    logging.info(f"The checked and parsed language requested is ==> {language} <==\n")
     entropy = 128                  # 128 bit represents 12 seed words
     mnem = Mnemonic(language)      # Assumes all generated seed phrases are valid.
 
-    phrase1 = mnem.generate(entropy)
+    obj_mnem1 = Bip39MnemonicGenerator().FromWordsNumber(Bip39WordsNum.WORDS_NUM_12)
+    logging.info(f"The bip_utils generated mnemonic 1 is : Type - {type(obj_mnem1)}\n\t{obj_mnem1}")
+    str_mnem1 = obj_mnem1.ToStr()
+    logging.info(f"The bip_utils generated mnemonic 1 is : Type - {type(str_mnem1)}")
+
+
     attempts = 0
+    validator = Bip39MnemonicValidator()
     valid = False
     while ( not valid ):
-           attempts = attempts + 1
-           phrase2 = mnem.generate(entropy)
-           phrase_dbl = phrase1 + " " + phrase2
-           valid = mnem.check(phrase_dbl)
+        attempts = attempts + 1
+        TEST_STRING = "Hello, world TEST."
+        print(f"TEST STRING IS OF TYPE {type(TEST_STRING)}")
+        # obj_mnem1 = Bip39MnemonicGenerator().FromWordsNumber(Bip39WordsNum.WORDS_NUM_12)
+        obj_mnem2 = Bip39MnemonicGenerator().FromWordsNumber(Bip39WordsNum.WORDS_NUM_12)
+        str_mnem2 = obj_mnem2.ToStr()
+        logging.info(f"The bip_utils generated mnemonic 2 is : Type - {type(str_mnem2)}")
+        print(f"\t\t The loop variable 'valid' is ==>{valid}<==")
+        str_dbl = str_mnem1 + " " + str_mnem2
+        logging.info(f"The double phrase at attempt number {attempts} is \n{str_dbl}")
+        logging.info(f"The expected string is of type {type(str_dbl)}")
+        valid = validator.IsValid(str_dbl)
+        print(f"\t\t The loop variable 'valid' is ==>{valid}<==")
 
-    seed_one = mnem.to_seed(phrase1, passphrase="")
-    seed_two = mnem.to_seed(phrase2, passphrase="")
-    seed_dbl = mnem.to_seed(phrase_dbl, passphrase="")
 
-    fp1 = calc_fingerprint(phrase1)
-    fp2 = calc_fingerprint(phrase2)
-    fpd = calc_fingerprint(phrase_dbl)
+    # seed_one = mnem.to_seed(obj_mnem1, passphrase="")
+    # seed_two = mnem.to_seed(obj_mnem2, passphrase="")
+    # seed_dbl = mnem.to_seed(phrase_dbl, passphrase="")
 
-    print(f"Seed phrase 1 = {phrase1}")
-    print(f"Seed: {seed_one.hex()} .")
-    print(f"\tFingerprint: {fp1} \n")
+    # fp1 = calc_fingerprint(obj_mnem1)
+    # fp2 = calc_fingerprint(obj_mnem2)
+    # fpd = calc_fingerprint(phrase_dbl)
 
-    print(f"Seed phrase 2 = {phrase2}")
-    print(f"Seed: {seed_two.hex()} .")
-    print(f"\tFingerprint: {fp2} \n")
+    print(f"Seed phrase 1 = {obj_mnem1}")
+    # print(f"Seed: {seed_one.hex()} .")
+    # print(f"\tFingerprint: {fp1} \n")
 
-    print(f"Double seed phrase = {phrase_dbl}")
-    print(f"Seed: {seed_dbl.hex()} .")
-    print(f"\tFingerprint: {fpd} \n")
+    print(f"Seed phrase 2 = {obj_mnem2}")
+    # print(f"Seed: {seed_two.hex()} .")
+    # print(f"\tFingerprint: {fp2} \n")
+
+    print(f"Double seed phrase = \n\t{obj_mnem1}\n\t{obj_mnem2}")
+    # print(f"Seed: {seed_dbl.hex()} .")
+    # print(f"\tFingerprint: {fpd} \n")
 
     print(f"\t\t\t{attempts} attempt(s) were made.\n")
 
